7f4f71f55d6f9edfee1ea23b59048787543a6e3a
adamjmcgrath <adam.mcgrath@auth0.com>
2020-04-01 13:44:21 +0100

    Initial commit

---
+++ sw.js
@@ -0,0 +1,16 @@
+importScripts('./node_modules/idb-keyval/dist/idb-keyval-iife.js');
+
+var secret;
+
+self.addEventListener('message', async function(e) {
+  const { data, ports: [ port ] } = e;
+  if (data.type === 'put') {
+    secret = data.secret;
+    idbKeyval.set('secret', secret);
+  } else if (data.type === 'get') {
+    port.postMessage({
+      global: secret,
+      idb: await idbKeyval.get('secret')
+    })
+  }
+});
