ebc9222e9292832caa08661f027c847add708527
adamjmcgrath <adam.mcgrath@auth0.com>
2020-04-01 13:45:20 +0100

    await set

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener('message', async function(e) {
   const { data, ports: [ port ] } = e;
   if (data.type === 'put') {
     secret = data.secret;
-    idbKeyval.set('secret', secret);
+    await idbKeyval.set('secret', secret);
   } else if (data.type === 'get') {
     port.postMessage({
       global: secret,
